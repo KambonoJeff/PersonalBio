c640cc5d45969a85a3843ddb4239948013ca6145
KAMBONO <kambonoj@gmail.com>
2023-04-22 10:39:58 +0300

    Added a PHP email sending script

---
+++ php/emailSender.php
@@ -0,0 +1,17 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phonenumber = $_POST['phonenumber'];
+$message = $_POST['message'];
+
+require "vendo/autoload.php" ;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+$mail = new PHPMailer(true);
+$mail->isSMTP();
+$mail->SMTPAuth = true ;
+
+$mail-> Host = "";
+$mail->SMTPSecure =PHPMailer::ENCRYPTION_STARTTLS;
+
+?>
